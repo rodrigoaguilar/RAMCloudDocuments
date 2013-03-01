1b9699dec55b36141a323aae115a18d237653463
Rodrigo Aguilar <rodrigo@rodrigoaguilar.com>
2013-03-01 15:22:09 -0300

    Remove keys for google drive and dropbox

--- RAMCloudDocumentsExample/RAMCloudDocumentsExampleViewController.h
+++ RAMCloudDocumentsExample/RAMCloudDocumentsExampleViewController.h
@@ -8,12 +8,12 @@
 
 #import <UIKit/UIKit.h>
 
-static NSString *const kGoogleDriveKeychainItemName = @"RAMCloudDocumentsExample";  //delete
-static NSString *const kGoogleDriveClientId = @"514956818484.apps.googleusercontent.com";  //delete
-static NSString *const kGoogleDriveClientSecret = @"dLw4nQcCdIknXAvWPRzHH4lf"; //delete
+static NSString *const kGoogleDriveKeychainItemName = @"RAMCloudDocumentsExample";  
+static NSString *const kGoogleDriveClientId = @"CLIENTID";
+static NSString *const kGoogleDriveClientSecret = @"CLIENTSECRET"; 
 
-static NSString *const kDropboxAppKey = @"hg89ipjoqkuucld";  //delete
-static NSString *const kDropboxAppSecret = @"042o4h0q6ntshr4";  //delete
+static NSString *const kDropboxAppKey = @"KEY";  
+static NSString *const kDropboxAppSecret = @"SECRET";  
 
 @interface RAMCloudDocumentsExampleViewController : UITableViewController <UITableViewDataSource, UITableViewDelegate>
 
